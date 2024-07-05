b9d563bddd38d259c2783d16b076aae10b128c19
Prasuk Jain <prasukjain@Prasuks-MacBook-Air.local>
2024-07-05 23:09:00 +0530

    hello

--- main.py
+++ main.py
@@ -9,8 +9,8 @@ from pygame import mixer
 pygame.init()
 
 # Declaring constants for screen size
-SCREEN_WIDTH = 800
-SCREEN_HEIGHT = 700
+SCREEN_WIDTH1 = 800
+SCREEN_HEIGHT1 = 700
 
 # Getting background image
 BACKGR1OUND = pygame.transform.smoothscale(pygame.image.load("spacebg.jpg"), (SCREEN_WIDTH, SCREEN_HEIGHT))
